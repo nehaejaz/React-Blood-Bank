143812a431b5bd2342cff6e2c0e8779e8678ccc5
nehaejaz <neha.ejaz@icloud.com>
2019-05-15 04:58:01 +0500

    inital commit

---
+++ src/components/auth/SignUp.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SignUp extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="SignUp">
+        <h1>Signup Component</h1>
+      </div>
+    );
+  }
+}
+export default SignUp;
