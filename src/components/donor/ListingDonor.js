d69d7cbf383052690b8aba187ad959518b599700
nehaejaz <neha.ejaz@icloud.com>
2019-05-16 00:26:58 +0500

    initail commit

---
+++ src/components/donor/ListingDonor.js
@@ -0,0 +1,11 @@
+import React from "react";
+import DonorSummary from "./DonorSummary";
+
+const ListingDonor = () => {
+  return (
+    <div className="ListingDonor">
+      <DonorSummary />
+    </div>
+  );
+};
+export default ListingDonor;
