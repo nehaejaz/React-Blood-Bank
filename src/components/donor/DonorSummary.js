08be47f417f9e5a494a51d9a99f8e5bb21a39dfa
nehaejaz <neha.ejaz@icloud.com>
2019-05-16 00:27:51 +0500

    component to show summary of each donor

---
+++ src/components/donor/DonorSummary.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+
+class DonorSummary extends Component {
+  render() {
+    return <div className="DonorSummary">Donor Summary</div>;
+  }
+}
+export default DonorSummary;
