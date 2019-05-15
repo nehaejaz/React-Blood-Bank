66815e824025ebebfbb112c51ef01ea883831f21
nehaejaz <neha.ejaz@icloud.com>
2019-05-16 00:27:08 +0500

    navigation bar

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import SignoutLinks from "./SignoutLinks";
+import SigninLinks from "./SigninLinks";
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <div class="nav-wrapper">
+          <Link to="/" className="brand-logo">
+            Blood Bank
+          </Link>
+          <SignoutLinks />
+          <SigninLinks />
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
