b8c4f07193890bb760ca1afdbaf43cdb9b084ea4
nehaejaz <neha.ejaz@icloud.com>
2019-05-16 16:21:23 +0500

    used card to layout the donars

--- src/components/donor/DonorSummary.js
+++ src/components/donor/DonorSummary.js
@@ -1,8 +1,22 @@
 import React, { Component } from "react";
+import { Col, Row, Button } from "react-materialize";
 
 class DonorSummary extends Component {
   render() {
-    return <div className="DonorSummary">Donor Summary</div>;
+    return (
+      <div className="DonorSummary">
+        <div className="">
+          <Row className="grey">
+            <Col s={3}>Neha</Col>
+            <Col s={3}>A+</Col>
+            <Col s={3}>Karachi,Pakistan</Col>
+            <Col s={3}>
+              <Button>View Details</Button>
+            </Col>
+          </Row>
+        </div>
+      </div>
+    );
   }
 }
 export default DonorSummary;
