39ba6a1d25c16b8925480d287c15d9b0eec3edb6
nehaejaz <neha.ejaz@icloud.com>
2019-05-16 16:25:38 +0500

    Modal added

--- src/components/donor/DonorSummary.js
+++ src/components/donor/DonorSummary.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Col, Row, Button } from "react-materialize";
+import { Col, Row, Button, Modal } from "react-materialize";
 
 class DonorSummary extends Component {
   render() {
@@ -11,7 +11,21 @@ class DonorSummary extends Component {
             <Col s={3}>A+</Col>
             <Col s={3}>Karachi,Pakistan</Col>
             <Col s={3}>
-              <Button>View Details</Button>
+              <Modal
+                header="Modal Header"
+                trigger={<Button>View Details</Button>}
+              >
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
+                  irure dolor in reprehenderit in voluptate velit esse cillum
+                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+                  cupidatat non proident, sunt in culpa qui officia deserunt
+                  mollit anim id est laborum
+                </p>
+              </Modal>
             </Col>
           </Row>
         </div>
