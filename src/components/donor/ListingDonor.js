a7fa04724ab036641e3290a339de9c6adc907c40
nehaejaz <neha.ejaz@icloud.com>
2019-05-16 16:20:51 +0500

    listing of users with card

--- src/components/donor/ListingDonor.js
+++ src/components/donor/ListingDonor.js
@@ -1,10 +1,37 @@
 import React from "react";
 import DonorSummary from "./DonorSummary";
+import Header from "./Header";
+import { Col, Row, Card } from "react-materialize";
 
 const ListingDonor = () => {
   return (
     <div className="ListingDonor">
-      <DonorSummary />
+      <Row>
+        <Col m={12} s={12}>
+          <Card
+            className="blue-grey darken-1 center"
+            textClassName="white-text"
+            title="Dashboard"
+          >
+            <Row>
+              <Col s={3} className=" white-text">
+                Name
+              </Col>
+              <Col s={3} className=" white-text">
+                BloodGroup
+              </Col>
+              <Col s={3} className=" white-text">
+                Location
+              </Col>
+              <Col s={3} className=" white-text">
+                Details
+              </Col>
+            </Row>
+            <DonorSummary />
+            <DonorSummary />
+          </Card>
+        </Col>
+      </Row>
     </div>
   );
 };
