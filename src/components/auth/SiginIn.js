9468b09c86ad74024215588a46d2190fd6d03467
nehaejaz <neha.ejaz@icloud.com>
2019-05-17 03:11:58 +0500

    fixed errors

--- src/components/auth/SiginIn.js
+++ src/components/auth/SiginIn.js
@@ -3,37 +3,58 @@ import { Row, Col, Card, Button } from "react-materialize";
 
 class SignIn extends Component {
   state = {
-    email: [],
-    password: []
+    email: "",
+    password: ""
+  };
+  ChangeHandler = e => {
+    this.setState({
+      [e.target.id]: e.target.value
+    });
+  };
+  SubmitHandler = e => {
+    e.preventDefault();
+    console.log(this.state);
   };
   render() {
     return (
-      <div className="SignUp center">
-        <Row>
-          <Col className="offset-m6" m={6} s={12}>
-            <Card
-              className="blue-grey darken-1"
-              textClassName="white-text"
-              title="Sign In"
-            >
-              <div class="row">
-                <div class="input-field col s12">
-                  <i class="material-icons prefix">contact_mail</i>
-                  <input id="email" type="email" class="validate" />
-                  <label for="email">Email</label>
+      <div className="SignIn center">
+        <form onSubmit={this.SubmitHandler}>
+          <Row>
+            <Col className="offset-m6" m={6} s={12}>
+              <Card
+                className="blue-grey darken-1"
+                textClassName="white-text"
+                title="Sign In"
+              >
+                <div className="row">
+                  <div className="input-field col s12">
+                    <i className="material-icons prefix">contact_mail</i>
+                    <input
+                      id="email"
+                      type="email"
+                      className="validate"
+                      onChange={this.ChangeHandler}
+                    />
+                    <label htmlFor="email">Email</label>
+                  </div>
                 </div>
-              </div>
-              <div class="row">
-                <div class="input-field col s12">
-                  <i class="material-icons prefix">lock</i>
-                  <input id="password" type="password" class="validate" />
-                  <label for="password">Password</label>
+                <div className="row">
+                  <div className="input-field col s12">
+                    <i className="material-icons prefix">lock</i>
+                    <input
+                      id="password"
+                      type="password"
+                      className="validate"
+                      onChange={this.ChangeHandler}
+                    />
+                    <label htmlFor="password">Password</label>
+                  </div>
                 </div>
-              </div>
-              <Button>Submit</Button>
-            </Card>
-          </Col>
-        </Row>
+                <Button type="submit">Submit</Button>
+              </Card>
+            </Col>
+          </Row>
+        </form>
       </div>
     );
   }
