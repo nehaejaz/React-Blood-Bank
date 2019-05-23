c28780d84c6c9bd50c1bbb3a2439a49f458632d8
nehaejaz <neha.ejaz@icloud.com>
2019-05-23 15:23:19 +0500

    accesing the donor state and displaying it

--- src/components/donor/DonorSummary.js
+++ src/components/donor/DonorSummary.js
@@ -1,36 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
 import { Col, Row, Button, Modal } from "react-materialize";
 
-class DonorSummary extends Component {
-  render() {
-    return (
-      <div className="DonorSummary">
-        <div className="">
-          <Row className="grey">
-            <Col s={3}>Neha</Col>
-            <Col s={3}>A+</Col>
-            <Col s={3}>Karachi,Pakistan</Col>
-            <Col s={3}>
-              <Modal
-                header="Modal Header"
-                trigger={<Button>View Details</Button>}
-              >
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                  laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                  irure dolor in reprehenderit in voluptate velit esse cillum
-                  dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                  cupidatat non proident, sunt in culpa qui officia deserunt
-                  mollit anim id est laborum
-                </p>
-              </Modal>
-            </Col>
-          </Row>
-        </div>
+const DonorSummary = ({ donors }) => {
+  return (
+    <div className="DonorSummary">
+      <div className="">
+        <Row className="grey">
+          <Col s={3}>{donors.name}</Col>
+          <Col s={3}>{donors.bloodgrp}</Col>
+          <Col s={3}>{donors.location}</Col>
+          <Col s={3}>
+            <Modal
+              header="Modal Header"
+              trigger={<Button>View Details</Button>}
+            >
+              <p>
+                {donors.id}
+                {donors.name}
+                {donors.bloodgrp}
+                {donors.location}
+              </p>
+            </Modal>
+          </Col>
+        </Row>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 export default DonorSummary;
