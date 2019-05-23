3d1a2a9307c5b61d790e04dd97f9463ebbe7df97
nehaejaz <neha.ejaz@icloud.com>
2019-05-23 15:23:45 +0500

    updated the init state

--- src/store/reducers/donorReducer.js
+++ src/store/reducers/donorReducer.js
@@ -1,4 +1,12 @@
-const donorReducer = (state, action) => {
+const initState = {
+  donors: [
+    { id: 1, name: "neha", bloodgrp: "A+", location: "khi" },
+    { id: 1, name: "bassam", bloodgrp: "B+", location: "lhr" },
+    { id: 1, name: "Anusha", bloodgrp: "O+", location: "isl" }
+  ]
+};
+
+const donorReducer = (state = initState, action) => {
   return state;
 };
 
