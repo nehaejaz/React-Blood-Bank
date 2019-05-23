b0c26dd40295e94cc3cdc6528550d27e3839356a
nehaejaz <neha.ejaz@icloud.com>
2019-05-23 20:58:40 +0500

    donor reducer updated with swtich and action type

--- src/store/reducers/donorReducer.js
+++ src/store/reducers/donorReducer.js
@@ -1,12 +1,19 @@
 const initState = {
   donors: [
     { id: 1, name: "neha", bloodgrp: "A+", location: "khi" },
-    { id: 1, name: "bassam", bloodgrp: "B+", location: "lhr" },
-    { id: 1, name: "Anusha", bloodgrp: "O+", location: "isl" }
+    { id: 2, name: "bassam", bloodgrp: "B+", location: "lhr" },
+    { id: 3, name: "Anusha", bloodgrp: "O+", location: "isl" }
   ]
 };
 
 const donorReducer = (state = initState, action) => {
+  switch (action.type) {
+    case "RegisteredDonor":
+      console.log("Donor has been registered");
+      break;
+    default:
+      console.log("default");
+  }
   return state;
 };
 
