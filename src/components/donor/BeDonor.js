632158b40f71edbc43755b82597ba3efe03a6ba4
nehaejaz <neha.ejaz@icloud.com>
2019-05-25 04:07:09 +0500

    added formik

--- src/components/donor/BeDonor.js
+++ src/components/donor/BeDonor.js
@@ -1,60 +1,89 @@
-import React, { Component } from "react";
-import { Row, Col, Card, Button } from "react-materialize";
+import React from "react";
 import "./css/bedonor.css";
+import { donorActions } from "../../store/actions/donorActions";
 import { connect } from "react-redux";
-import { donorAction } from "../../store/actions/donorActions";
-class BeDonor extends Component {
-  state = {
-    email: "",
-    password: ""
-  };
-  ChangeHandler = e => {
-    this.setState({
-      [e.target.id]: e.target.value
-    });
-  };
-  SubmitHandler = e => {
-    e.preventDefault();
-    this.props.registerDonor(this.state);
-  };
-  render() {
-    return (
-      <div className="Bedonor center">
-        <form onSubmit={this.SubmitHandler}>
+import { Card, Col, Row, Button } from "react-materialize";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+
+const BeDonor = props => (
+  <div className="Bedonor">
+    <Formik
+      initialValues={{
+        FirstName: "",
+        LastName: "",
+        Age: "",
+        BloodGroup: "",
+        Contact: "",
+        City: "",
+        Country: ""
+      }}
+      validate={values => {
+        let errors = {};
+        if (!values.FirstName) {
+          errors.FirstName = "Required";
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.FirstName)
+        ) {
+          errors.FirstName = "Invalid email address";
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        // setTimeout(() => {
+        // alert(JSON.stringify(values, null, 2));
+        props.RegistereDonor(values);
+        setSubmitting(false);
+        // }, 400);
+      }}
+    >
+      {({ isSubmitting }) => (
+        <Form>
           <Row className="center">
-            <Col className="offset-m3" m={6} s={12}>
+            <Col className="offset-m2" m={8} s={12}>
               <Card
                 className="red lighten-2 bedonor-card"
                 textClassName="white-text"
                 title="Become a Donor"
               >
                 <div className="row">
-                  <div className="input-field col s12">
+                  <div className="input-field col s6">
                     <i className="material-icons prefix">contact_mail</i>
-                    <input
-                      id="email"
-                      type="email"
-                      className="validate"
-                      onChange={this.ChangeHandler}
+                    <Field
+                      type="text"
+                      name="FirstName"
+                      placeholder="FirstName"
                     />
-                    <label htmlFor="email">Email</label>
+                    <ErrorMessage name="FirstName" component="div" />
+                  </div>
+                  <div className="input-field col s6">
+                    <i className="material-icons prefix">contact_mail</i>
+                    <Field type="text" name="LastName" placeholder="LastName" />
+                    <ErrorMessage name="LastName" component="div" />
                   </div>
                 </div>
-                <Button type="submit">Register</Button>
+                {/* <button type="submit" disabled={isSubmitting}>
+                  Submit
+                </button> */}
+                <Button type="submit" disabled={isSubmitting}>
+                  Register
+                </Button>
               </Card>
             </Col>
           </Row>
-        </form>
-      </div>
-    );
-  }
-}
+          {/* <Field type="password" name="password" />
+          <ErrorMessage name="password" component="div" /> */}
+        </Form>
+      )}
+    </Formik>
+  </div>
+);
 
 const mapDispatchToProps = dispatch => {
   return {
-    registerDonor: donor => dispatch(donorAction(donor))
+    RegistereDonor: donor => dispatch(donorActions(donor))
   };
 };
+
 export default connect(
   null,
   mapDispatchToProps
