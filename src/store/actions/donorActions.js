58f341ac0e601bdb53a45bde00fbe1493f9b7983
nehaejaz <neha.ejaz@icloud.com>
2019-05-25 04:08:00 +0500

    writing the sate in firestore

--- src/store/actions/donorActions.js
+++ src/store/actions/donorActions.js
@@ -1,6 +1,17 @@
-export const donorAction = donor => {
+export const donorActions = donor => {
   return (dispatch, getState, { getFiresbase, getFirestore }) => {
-    //aysn call to DB
-    dispatch({ type: "RegisteredDonor", donor: donor });
+    //aysn call to store donor information in RegisterDonor Collection
+    const firestore = getFirestore();
+    firestore
+      .collection("RegisteredDonors")
+      .add({
+        ...donor
+      })
+      .then(() => {
+        dispatch({ type: "RegisteredDonor", donor: donor });
+      })
+      .catch(err => {
+        dispatch({ type: "Error_RegisterDonor", err });
+      });
   };
 };
